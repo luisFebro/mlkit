d8d86870f919f36276828d8e7ee5af271204b2b3
Luis Febro <mr.febro@gmail.com>
2020-09-08 22:05:17 -0400

    knn testing accuracy

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,5 @@
 const outputs = [];
 
-const predictionPoint = 300;
 const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
@@ -8,8 +7,35 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-    const bucket = _.chain(outputs)
-        .map(row => [getDistance(row[0]), row[3]])
+    const testSetSize = 10;
+    const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
+    // testSet the first chunk of data limit by testSetSize;
+    // trainingSet - the whole other data: 720
+
+    const accuracy = _.chain(testSet) //  n1 refactored from
+        .filter(testPoint => getKnn(trainingSet, testPoint[0]) === testPoint[3])
+        .size()
+        .divide(testSetSize)
+        .value()
+
+    console.log("Accuracy: ", accuracy);
+}
+
+
+function splitDataSet(data, testCount) {
+    const shuffled = _.shuffle(data);
+
+    const dataTest = _.slice(data, 0, testCount);
+    const trainingSet = _.slice(data, testCount);
+
+    return [dataTest, trainingSet];
+}
+
+// HELPERS
+// KNN - K Nearest neighbors Algorithm
+function getKnn(data, point) {
+    return _.chain(data)
+        .map(row => [getDistance(row[0], point), row[3]])
         .sortBy(row => row[0])
         .slice(0, k)
         .countBy(row => row[1])
@@ -19,10 +45,25 @@ function runAnalysis() {
         .first()
         .parseInt()
         .value()
-
-    console.log(bucket);
 }
 
-const getDistance = (point) => {
-    return Math.abs(point - predictionPoint);
+// pointB - predictionPoint
+function getDistance(pointA, pointB) {
+    return Math.abs(pointA - pointB);
 }
+// END HELPERS
+
+
+/* COMMENTS
+n1:
+// let numberCorrect = 0;
+// for(let i = 0; i < testSet.length; i++) {
+//     const predictionPoint = testSet[i][0];
+//     const bucket = getKnn(trainingSet, predictionPoint);
+//     const testBucket = testSet[i][3];
+
+//     if(bucket === testBucket) {
+//         numberCorrect++;
+//     }
+// }
+*/
